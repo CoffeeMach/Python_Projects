8b21a8f40f9e60f148f2658360eaf02e355b7811
CoffeeMach <you@example.com>
2026-07-08 06:47:48 +0900

    Implemented bubblesort, need to test

--- davincicode.py
+++ davincicode.py
@@ -4,7 +4,6 @@ class Tiles:
     def __init__(self, color, number):
         self.color = color
         self.number = number
-        self.is_scramble = False
 
     def __str__(self):
         return f"[{self.color}{self.number}]"
@@ -15,17 +14,17 @@ class Tiles:
     def reveal(self):
         print(self.number)
 
-def coda_sorting_algo(list):
-    for index in range(len(list)):
-        if list[index] < list[index+1]:
-            list
-        elif list[index] > list[index+1]:
-            temp = list[index+1]
-            list[index+1] = list[index]
-            list[index] = temp
-            coda_sorting_algo[list]
+def coda_sorting_algo(lst):
+    for index in range(len(lst)-1):
+        if lst[index].number <= lst[index+1].number:
+            continue
+        elif lst[index].number > lst[index+1].number:
+            temp = lst[index+1]
+            lst[index+1] = lst[index]
+            lst[index] = temp
+            coda_sorting_algo(lst)
 
-    return list
+    return lst
 
 def initialize_game():
     rng = np.random.default_rng()
@@ -56,10 +55,9 @@ def setup(shuffled_tiles_list):
     
     remaining_tiles = np.delete(other_tiles, [0,1,2,3,4,5,6,7])
 
-    my_tiles = coda_sorting_algo(my_tiles)
+    # my_tiles = coda_sorting_algo(my_tiles)
     player2_tiles = coda_sorting_algo(player2_tiles)
     player3_tiles = coda_sorting_algo(player3_tiles)
-    remaining_tiles = coda_sorting_algo(remaining_tiles)
 
     print(f" Your tiles: {my_tiles}\n",f"Tiles of p2: {player2_tiles}\n",
           f"Tiles of p3: {player3_tiles}\n\n",f"Remaining tiles: {remaining_tiles}")
