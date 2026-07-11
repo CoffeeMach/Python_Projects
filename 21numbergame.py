7fe8bef2cf19a8bebc4e22ba70717915c017428c
CoffeeMach <you@example.com>
2026-07-12 07:56:50 +0900

    Fixed game loop logic with lists

--- 21numbergame.py
+++ 21numbergame.py
@@ -7,45 +7,61 @@ def description():
     "If you are the one to call 21, you lose! Else, you win!\n" \
     "Good luck and may the odds be ever in your favor!\n")
 
-def game_loop():
-    called_nums = [0]
+def player_loop(player_turn):
+    called_nums = [0]  # start with zero to get last_num
 
-    last_num = called_nums[len(called_nums)-1]
+    last_num = called_nums[len(called_nums)-1]  # check for consecutiveness
 
-    player_nums = input("Call 1 to 3 consecutive numbers: ").split()
+    if (last_num < 21 and player_turn):
 
-    if not player_nums:
-        print("You must enter at least one number!")
-    elif len(player_nums) > 3:
-        print("You are disqualified!")
-        return
-    else:
-        num1 = int(player_nums[0])
-        if num1 != last_num + 1:
+        last_num = called_nums[len(called_nums)-1]
+
+        player_nums = input("Call 1 to 3 consecutive numbers: ").split()
+
+        if not player_nums:
+            print("You must enter at least one number!")
+        elif len(player_nums) > 3:
             print("You are disqualified!")
             return
-        if len(player_nums) > 1:
-            num2 = int(player_nums[1])
-            if num2 != num1 + 1:
+        else:
+            num1 = int(player_nums[0])
+            if num1 != last_num + 1:
                 print("You are disqualified!")
                 return
-        else:
-            num2 = None
-        if len(player_nums) > 2:
-            num3 = int(player_nums[2])
-            if num3 != num2 + 1:
-                print("You are disqualified!")
-                return
-        else:
-            num3 = None
+            else:
+                called_nums.extend([num1])
+            if len(player_nums) > 1:
+                num2 = int(player_nums[1])
+                if num2 != num1 + 1:
+                    print("You are disqualified!")
+                    return
+                else:
+                    called_nums.extend([num2])
+            else:
+                num2 = None
+            if len(player_nums) > 2:
+                num3 = int(player_nums[2])
+                if num3 != num2 + 1:
+                    print("You are disqualified!")
+                    return
+                else:
+                    called_nums.extend([num3])
+            else:
+                num3 = None
 
-    called_nums.extend([num1, num2, num3])
-    print(called_nums)
+        print(called_nums)
 
+    return player_turn
+
+def computer_loop(computer_turn):
+    return computer_turn
+
+def game_loop(player_loop, computer_loop):
+    player_turn = player_loop()
+    computer_turn = computer_loop()
 
 def main():
     description()
-    game_loop()
 
 if __name__ == "__main__":
     main()
