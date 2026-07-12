5acc7517753f050d042e7cdf120babc13b51d6e1
CoffeeMach <you@example.com>
2026-07-12 20:11:34 +0900

    First working version with minor faults

--- 21numbergame.py
+++ 21numbergame.py
@@ -1,3 +1,5 @@
+import random
+
 def description():
     print("Welcome to 21 Number Game!\n" \
     "The objective of this game is to not be the one to call 21.\n" \
@@ -7,61 +9,99 @@ def description():
     "If you are the one to call 21, you lose! Else, you win!\n" \
     "Good luck and may the odds be ever in your favor!\n")
 
-def player_loop(player_turn):
-    called_nums = [0]  # start with zero to get last_num
+    starting_player = input("Do you want to go first or second? [F/S]: ")
 
-    last_num = called_nums[len(called_nums)-1]  # check for consecutiveness
+    match starting_player:
+        case "F":
+            player_turn = True
+            computer_turn = False
+        case "S":
+            computer_turn = True
+            player_turn = False
+        case _:
+            return
+        
+    return player_turn, computer_turn
 
-    if (last_num < 21 and player_turn):
+def player_loop(called_nums, last_num):
+    player_nums = input("Call 1 to 3 consecutive numbers: ").split()
 
-        last_num = called_nums[len(called_nums)-1]
-
-        player_nums = input("Call 1 to 3 consecutive numbers: ").split()
-
-        if not player_nums:
-            print("You must enter at least one number!")
-        elif len(player_nums) > 3:
+    if not player_nums:
+        print("You must enter at least one number!")
+    elif len(player_nums) > 3:
+        print("You are disqualified!")
+        return
+    else:
+        num1 = int(player_nums[0])
+        if num1 != last_num + 1:
             print("You are disqualified!")
             return
         else:
-            num1 = int(player_nums[0])
-            if num1 != last_num + 1:
+            called_nums.extend([num1])
+        if len(player_nums) > 1:
+            num2 = int(player_nums[1])
+            if num2 != num1 + 1:
                 print("You are disqualified!")
                 return
             else:
-                called_nums.extend([num1])
-            if len(player_nums) > 1:
-                num2 = int(player_nums[1])
-                if num2 != num1 + 1:
-                    print("You are disqualified!")
-                    return
-                else:
-                    called_nums.extend([num2])
+                called_nums.extend([num2])
+        else:
+            num2 = None
+        if len(player_nums) > 2:
+            num3 = int(player_nums[2])
+            if num3 != num2 + 1:
+                print("You are disqualified!")
+                return
             else:
-                num2 = None
-            if len(player_nums) > 2:
-                num3 = int(player_nums[2])
-                if num3 != num2 + 1:
-                    print("You are disqualified!")
-                    return
-                else:
-                    called_nums.extend([num3])
-            else:
-                num3 = None
+                called_nums.extend([num3])
+        else:
+            num3 = None
 
-        print(called_nums)
+    player_turn = False
+    computer_turn = True
 
-    return player_turn
+    return last_num, player_turn, computer_turn, called_nums
 
-def computer_loop(computer_turn):
-    return computer_turn
+def computer_loop(called_nums, last_num):
+    next_num = last_num + 1
+    number_of_nums = random.randint(1,3)
 
-def game_loop(player_loop, computer_loop):
-    player_turn = player_loop()
-    computer_turn = computer_loop()
+    if (number_of_nums == 1):
+        called_nums.extend([next_num])
+    elif (number_of_nums == 2):
+        called_nums.extend([next_num, next_num + 1])
+    elif (number_of_nums == 3):
+        called_nums.extend([next_num, next_num + 1, next_num + 2])
+
+    computer_turn = False
+    player_turn = True
+
+    return last_num, computer_turn, player_turn, called_nums
+
+def end_condition(last_num, player_turn, computer_turn):
+    if (last_num >= 21 and player_turn):
+        print("You lose!")
+    elif (last_num >= 21 and computer_turn):
+        print("You win!")
+
+def game_loop(player_turn, computer_turn):
+    called_nums = [0]  # start with zero to get last_num
+    last_num = called_nums[len(called_nums)-1] # check for consecutiveness
+
+    while (last_num < 21):
+        last_num = called_nums[len(called_nums)-1]
+        if (player_turn):
+            last_num, player_turn, computer_turn, called_nums = player_loop(called_nums, last_num)
+            print(called_nums)
+        elif (computer_turn):
+            last_num, computer_turn, player_turn, called_nums = computer_loop(called_nums, last_num)
+            print(called_nums)
+        else:
+            end_condition(last_num, player_turn, computer_turn)
 
 def main():
-    description()
+    player_turn, computer_turn = description()
+    game_loop(player_turn, computer_turn)
 
 if __name__ == "__main__":
     main()
